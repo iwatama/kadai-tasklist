d8ff1732ae30f4d62dac3f8ba2662e110d61e6a8
iwata <iwaking43@gmail.com>
2017-04-25 00:28:22 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
     end
     
     def task_params
-        params.require(:task).permit(:content, :title)
+        params.require(:task).permit(:content, :title, :status)
     end
     
 end
