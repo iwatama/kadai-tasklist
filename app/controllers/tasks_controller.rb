52a83edd90ac4178e5f3d7ff5ed38c689fff9c22
iwata <iwaking43@gmail.com>
2017-04-26 15:36:43 +0000

    delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    ##before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     def index
         @tasks = Task.all.page(params[:page]).per(2)
     end
@@ -13,13 +15,23 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        # @task = Task.new(task_params)
+        # if @task.save
+        #     flash[:success] = 'taskが正常に登録されました'
+        #     redirect_to @task
+        # else
+        #     flash.now[:danger] = 'taskが登録失敗！！'
+        #     render :new
+        # end
+        
+        @task = current_user.tasks.build(task_params)
         if @task.save
             flash[:success] = 'taskが正常に登録されました'
-            redirect_to @task
+            redirect_to root_url
         else
-            flash.now[:danger] = 'taskが登録失敗！！'
-            render :new
+            @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+            flash.now[:danger] = 'taskの投稿に失敗しました。'
+            render 'toppages/index'
         end
     end
     
@@ -37,10 +49,14 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        # @task = Task.find(params[:id])
+        # # @task = Task.find(params[:id])
+        # @task.destroy
+        # flash[:success] = 'task delete success'
+        # redirect_to tasks_url
+        
         @task.destroy
         flash[:success] = 'task delete success'
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end
   
   
@@ -53,4 +69,10 @@ class TasksController < ApplicationController
         params.require(:task).permit(:content, :title, :status)
     end
     
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+          redirect_to root_path
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426002541) do
+ActiveRecord::Schema.define(version: 20170426142125) do
+
+  create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id", using: :btree
+  end
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -18,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170426002541) do
     t.datetime "updated_at", null: false
     t.string   "title"
     t.string   "status"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -28,4 +38,6 @@ ActiveRecord::Schema.define(version: 20170426002541) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "microposts", "users"
+  add_foreign_key "tasks", "users"
 end
